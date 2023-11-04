715c9d9c57feaa904c8752427614b02bf08dfe75
callumberry <callumjberry359@gmail.com>
2023-11-03 21:28:17 -0400

    added ip stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    proxy: {
+      //change ip to current ip address
+      '/api': 'http://192.168.2.113:5000' // Proxy requests to your Flask backend
+    },
+  },
 })
