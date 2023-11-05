7845d6e62bc932357cf8e2aaf9f764f145e0ebfe
callumberry <callumjberry359@gmail.com>
2023-11-05 11:26:33 -0500

    turned on functions

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,8 @@ from flask import request
 import ntplib
 from time import ctime
 from apscheduler.schedulers.background import BackgroundScheduler
-#from ledControl import toggle_led
-#from servoControl import move_servo_min_to_max
+from ledControl import toggle_led
+from servoControl import move_servo_min_to_max
 timeOne = None
 timeTwo = None
 portions = 1
@@ -38,7 +38,7 @@ def get_data():
 
 @app.route('/api/backend-action', methods=['GET'])
 def perform_backend_action():
-    #toggle_led()
+    toggle_led()
     print("LED is ON")
     return jsonify({'message': 'Led toggled'})
 
@@ -49,7 +49,7 @@ def perform_servo_action():
     print(repeat)
     # Perform the servo action 'repeat' times based on the sliderValue
     for _ in range(repeat):
-        #move_servo_min_to_max()
+        move_servo_min_to_max()
         print("Servo Moved")
 
     return jsonify({'message': f'Servo Positioned {repeat} times'})\
@@ -83,7 +83,7 @@ def schedule_job():
         print(repeat)
         # Perform the servo action 'repeat' times based on the sliderValue
         for _ in range(repeat):
-            #move_servo_min_to_max()
+            move_servo_min_to_max()
             print("Servo Moved")
         print("Performing scheduled action at", current_time)
     else:
